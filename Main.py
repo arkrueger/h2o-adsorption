a7364e8699921cf51e2ee08e65accd4dbec920fb
Alex Krueger <arkrueger@ucdavis.edu>
2019-12-27 12:47:34 -0800

    Initial upload.

---
+++ Main.py
@@ -0,0 +1,22 @@
+'''
+Alex Krueger
+2019
+Numerical Solutions for Modeling H2O Adsorption on a Silica Column
+
+Key Concepts:
+    Numerical Solutions
+    Finite Difference Methods
+    Dependent Partial Differential Equations
+'''
+
+### Background:
+'''This program uses a finite difference method to solve two linked partial differential equations describing the gas 
+phase (diffusion) and solid phase (adsorption) behavior.
+
+Equation 1: dG/dtau = -dG/dZ - kappa*(G - S)
+Equation 2: dS/dtau = psi*kappa*(G - S)
+
+
+'''
+
+
