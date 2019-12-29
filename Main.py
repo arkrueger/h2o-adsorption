3f4e0dd1c6ca224a76f5b7120fd6ff683262cee0
Alex Krueger <arkrueger@ucdavis.edu>
2019-12-28 21:04:10 -0800

    Load in s_matrices. (pandas DataFrame in object list because each trial has different dimensions.)

--- Main.py
+++ Main.py
@@ -17,7 +17,23 @@ phase (diffusion) and solid phase (adsorption) behavior.
 Equation 1: dG/dtau = -dG/dZ - kappa*(G - S)
 Equation 2: dS/dtau = psi*kappa*(G - S)
 
-
 '''
+import numpy as np
+import pandas as pd
+
+### Reading the data in.
+# The experiment consists of four configurations, each conducted with trials in triplicate.
+# HF = high flow, LF = low flow, C = with cooling jacket, N = no cooling jacket, 1/2/3 = trial number
+configs = ['HFC', 'HFN', 'LFC', 'LFN']
+trials = ['1', '2', '3']
+
+# Reading these in as pandas DataFrames and storing each as an object element in a list.
+# This method is better suited than MultiIndex, xarray, or 3D numpy array because the matrices have varying dimensions.
+s_matrices = [[pd.read_csv('Resources\\'+configs[k]+trials[i]+'.csv') for i in range(0,3)] for k in range(0,4)]
+
+
+
+
+
 
 
